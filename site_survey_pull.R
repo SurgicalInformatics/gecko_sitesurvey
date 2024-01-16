418f03a40044d15eb23dc59cd923e4c6e6b3d2e4
riinuots <riinuots@gmail.com>
2024-01-16 19:38:35 +0000

    lap vs open map and table added

--- site_survey_pull.R
+++ site_survey_pull.R
@@ -1,9 +1,4 @@
 library(tidyverse)
-# site_survey_orig = REDCapR::redcap_read(redcap_uri = Sys.getenv("redcap_globalsurg_uri"),
-#                                         token      = Sys.getenv("gecko_teamreg_token"),
-#                                         forms      = 'site_survey',
-#                                         records    = 'ae_1385'
-# )$data
 
 site_survey_orig <- list("token"=Sys.getenv("gecko_teamreg_token"),
                          content='report',
@@ -21,6 +16,29 @@ site_survey_orig <- list("token"=Sys.getenv("gecko_teamreg_token"),
   httr::POST(Sys.getenv("redcap_globalsurg_uri"), body = ., encode = "form") %>% 
   httr::content()
 
+# pulling labels instead of applying a factoring script
+site_survey_labels <- list("token"=Sys.getenv("gecko_teamreg_token"),
+                         content='report',
+                         action='export',
+                         format='csv',
+                         type='flat',
+                         csvDelimiter='',
+                         report_id= 539,
+                         rawOrLabel='label',
+                         rawOrLabelHeaders='label',
+                         exportCheckboxLabel='false',
+                         exportSurveyFields='false',
+                         exportDataAccessGroups='false',
+                         returnFormat='json') %>% 
+  httr::POST(Sys.getenv("redcap_globalsurg_uri"), body = ., encode = "form") %>% 
+  httr::content()
+
+variables = colnames(site_survey_orig)
+labels = colnames(site_survey_labels)
+
+names(labels) = variables
+
+# join with World Bank classifications
 wb = read_csv("data_processed/world_bank_classifications2024.csv", na = "") %>% 
   mutate(wb = fct_relevel(wb, "High income", "Upper middle income", "Lower middle income"))
 
@@ -28,7 +46,8 @@ n_records0 = n_distinct(site_survey_orig$record_id)
 
 site_survey_orig = site_survey_orig %>% 
   mutate(iso2 = toupper(str_sub(record_id, 1, 2))) %>% 
-  left_join(wb)
+  left_join(wb) %>% 
+  finalfit::ff_relabel(labels) # apply labels
 
 n_records1 = n_distinct(site_survey_orig$record_id)
 stopifnot(n_records0 == n_records1)
--- world_map.R
+++ world_map.R
@@ -4,8 +4,8 @@ library(maps)
 library(countrycode)
 theme_set(theme_bw())
 
-# load("patient_data_orig.rda")
-# load("registrations_data_orig.rda")
+#load("patient_data_orig.rda")
+#load("registrations_data_orig.rda")
 
 reg_summary = registrations_orig %>% 
   mutate(country = case_when(str_detect(data_collection_dag, "_gaza_") ~ "Gaza",
@@ -13,14 +13,33 @@ reg_summary = registrations_orig %>%
                              .default = country)) %>% 
   count(country, iso2, region, wb, name = "n_registrations")
 
+op_approach = patient_data %>% 
+  drop_na(op_approach) %>% 
+  mutate(country = case_when(str_detect(redcap_data_access_group, "_gaza_") ~ "Gaza",
+                             country == "West Bank and Gaza"                ~ "West Bank",
+                             .default = country)) %>% 
+  mutate(op_approach.2groups = fct_collapse(op_approach,
+                                            "Lap/Rob/Conv" = c("Laparoscopic",
+                                                               "Robotic",
+                                                               "Open conversion"))) %>% 
+  count(country, op_approach.2groups, .drop = FALSE) %>% 
+  group_by(country) %>% 
+  mutate(total = sum(n),
+         prop = round(n/total, 2)) %>% 
+  filter(op_approach.2groups == "Lap/Rob/Conv") %>% 
+  arrange(prop) %>% 
+  ungroup() %>% 
+  select(mapname = country, n_laprob = n, n_op_approach = total, prop_laprob = prop)
+
          
 country_summary = patient_data_orig %>% 
   mutate(country = case_when(str_detect(redcap_data_access_group, "_gaza_") ~ "Gaza",
-                             country == "West Bank and Gaza" ~ "West Bank",
+                             country == "West Bank and Gaza"                ~ "West Bank",
                              .default = country)) %>% 
   count(country, iso2, region, wb, name = "n_patients") %>% 
   full_join(reg_summary) %>% 
   rename(continent = region, mapname = country) %>% 
+  left_join(op_approach) %>% 
   # mapnames no longer used in matching
   mutate(mapname = fct_recode(mapname,
                              "USA"         = "United States",
@@ -44,7 +63,6 @@ country_summary = patient_data_orig %>%
                            )) %>% 
   mutate(n_patients = if_else(is.na(n_patients), 0, n_patients),
          n_registrations = if_else(is.na(n_registrations), 0, n_registrations))
-
 
 map_patients = country_summary %>%
   mutate(has_data = if_else(n_patients > 0, 1, 0)) %>% 
@@ -89,3 +107,24 @@ map_registrations = country_summary %>%
   hc_colorAxis(minColor = "#de6a35", maxColor = "#de6a35") %>%
   hc_chart(zoomType = "xy")
 
+map_op_approach = country_summary %>%
+  mutate(perc = scales::percent(prop_laprob, 1)) %>% 
+  #mutate(has_data = if_else(n_registrations > 0, 1, 0)) %>% 
+  hcmap(
+    map = "custom/world-palestine-highres", # high resolution world map
+    # data = int_map_data, # name of dataset
+    joinBy = "iso-a2",
+    value = "prop_laprob",
+    #showInLegend = FALSE, # hide legend
+    nullColor = "white",
+    download_map_data = TRUE,
+    name = "",
+    borderColor = "black", borderWidth = 0.1,
+    tooltip = list(pointFormat = "{point.name}: {point.n_laprob}/{point.n_op_approach} ({point.perc}) Lap+Rob/total")
+  ) %>%
+  hc_mapView(projection = "EqualEarth") %>% 
+  hc_mapNavigation(enabled = FALSE) %>%
+  hc_legend("none") %>%
+  #hc_title(text = "Gecko patient data entry") %>%
+  #hc_colorAxis(minColor = "#de6a35", maxColor = "#de6a35") %>%
+  hc_chart(zoomType = "xy")
